6b9b53c7a0be5b38257118e7d8c374810e7d13be
Peter Raboud <praboud@gmail.com>
2015-05-23 09:15:38 -0400

    Add smoke test of LSSB function

--- src/init_task_test.c
+++ src/init_task_test.c
@@ -2,13 +2,14 @@
 #include "init_task.h"
 #include "io.h"
 #include "util.h"
+#include "least_significant_set_bit.h"
 
 #define STRINGIFY2(STR) #STR
 #define STRINGIFY1(STR) STRINGIFY2(STR)
 
 #define ASSERT(stmt) {\
     if (!(stmt)) { \
-        io_puts(COM2, "ASSERTION FAILED (" __FILE__ ":" STRINGIFY1(__LINE__) ") : " STR1(stmt)); \
+        io_puts(COM2, "ASSERTION FAILED (" __FILE__ ":" STRINGIFY1(__LINE__) ") : " STR1(stmt) EOL); \
         io_flush(COM2); \
     } }
 
@@ -23,7 +24,17 @@ void nop(void) {
 
 const int init_task_priority = PRIORITY_MAX;
 
+void lssb_tests(void) {
+    int i;
+    ASSERT(0 == least_significant_set_bit(0xffffffff));
+    ASSERT(2 == least_significant_set_bit(0xc));
+    for (i = 0; i < 32; i++) {
+        ASSERT(i == least_significant_set_bit(0x1 << i));
+    }
+}
+
 void init_task(void) {
+    lssb_tests();
     ASSERT(1);
     ASSERT(create(-1, child) == CREATE_INVALID_PRIORITY);
     ASSERT(create(32, child) == CREATE_INVALID_PRIORITY);
