6b5ab6807c3745c90ca0910d1ad10701419d540c
crazy2be <code@jmcgirr.com>
2015-05-27 18:07:14 -0400

    Add a microseconds function, who knows if it works?

--- kernel/drivers/timer_qemu.c
+++ kernel/drivers/timer_qemu.c
@@ -44,6 +44,9 @@ int time_minutes(long long time) {
 int time_seconds(long long time) {
 	return time / TIME_SECOND % 60;
 }
+int time_useconds(long long time) {
+	return (time*1000000) / TIME_SECOND % 1000000;
+}
 int time_fraction(long long time) {
 	return time % TIME_SECOND;
 }
