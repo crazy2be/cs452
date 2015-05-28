a53d157cfdee44bee876644b461e1a529dd18d3c
crazy2be <code@jmcgirr.com>
2015-05-28 11:22:33 -0400

    Add team name.

--- writeup/k2/timings.py
+++ writeup/k2/timings.py
@@ -10,13 +10,16 @@ values = [85576, 183622, 57698, 116555, 82515, 180606, 55978, 114805,
 
 USE_LATEX = len(sys.argv) > 1 and sys.argv[1] == 'latex'
 if USE_LATEX: print '\\begin{tabular}{l|l|l|l||r}'
-print ['\t', '&'][USE_LATEX].join([name[0] for name in names] + ["Speed (us)"]),
+print ['\t', '&'][USE_LATEX].join([name[0] for name in names]
+								  + ([] if USE_LATEX else ["Team"])
+								  + ["Speed (us)"]),
 if USE_LATEX: print '\\\\',
 print
 
 for i, val in enumerate(values):
 	print ['\t', '&'][USE_LATEX].join(
 		[names[j][1 + i / (2**j) % 2] for j, name in enumerate(names)]
+			+ ([] if USE_LATEX else ["Peter and Justin"])
 			+ [str(values[i]/200)]),
 	if USE_LATEX: print '\\\\',
 	print
