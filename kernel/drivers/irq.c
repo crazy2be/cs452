ff36da04c6bf7467ec814246128df92691660ac1
Peter Raboud <praboud@gmail.com>
2015-05-30 18:16:22 -0400

    Experimental TS7200 support for IRQ

--- kernel/drivers/irq.c
+++ kernel/drivers/irq.c
@@ -1,7 +1,21 @@
 #include "irq.h"
 #include <io.h>
 
+// TS7200 has 64 possible interrupts, where as we only support the first 32
+// on versatile PB. Depending on which specific interrupts we need to listen
+// to, we may need to support more on the versatile PB later.
+
 #define VWRITE(addr, data) *(volatile unsigned *)(addr) = (data)
+
+#ifdef QEMU
+#else
+#define VIC1_BASE 0x800b0000
+#define VIC2_BASE 0x800c0000
+
+#define ENABLE_OFFSET 0x10
+#define SOFTINT_OFFSET 0x18
+#define SOFTINT_CLEAR_OFFSET 0x1c
+#endif
 
 void setup_irq(void) {
     // TODO: see A.2.6.8 in the arm manual
@@ -18,9 +32,9 @@ void setup_irq(void) {
     // PICIntEnable
     VWRITE(0x10140010, 0x0000ffff);
 #else
-    // writing to VICxIntEnable (see ep93xx user manual)
-    VWRITE(0x800b0010, 0xffffffff);
-    VWRITE(0x800c0010, 0xffffffff);
+    // writing to VIC1IntEnable & VIC2IntEnable (see ep93xx user manual)
+    VWRITE(VIC1_BASE + ENABLE_OFFSET, 0xffffffff);
+    VWRITE(VIC2_BASE + ENABLE_OFFSET, 0xffffffff);
 #endif
 }
 
@@ -28,7 +42,8 @@ void clear_irq(unsigned interrupts_c) {
 #ifdef QEMU
     VWRITE(0x1014001c, interrupts_c);
 #else
-    ASSERT(0);
+    VWRITE(VIC1_BASE + SOFTINT_CLEAR_OFFSET, interrupts_c);
+    VWRITE(VIC2_BASE + SOFTINT_CLEAR_OFFSET, interrupts_c);
 #endif
 }
 
@@ -36,7 +51,8 @@ void set_irq(unsigned interrupts) {
 #ifdef QEMU
     VWRITE(0x10140018, interrupts);
 #else
-    ASSERT(0);
+    VWRITE(VIC1_BASE + SOFTINT_OFFSET, interrupts);
+    VWRITE(VIC1_BASE + SOFTINT_OFFSET, interrupts);
 #endif
 }
 
@@ -44,6 +60,7 @@ unsigned get_irq(void) {
 #ifdef QEMU
     return *(volatile unsigned*) 0x10140000;
 #else
-    ASSERT(0);
+    // TODO: DTRT
+    return 0xbeefbeef;
 #endif
 }
