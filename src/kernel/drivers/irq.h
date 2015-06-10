43caef4e725af248265d1f97c52389abb6bd3c0a
crazy2be <code@jmcgirr.com>
2015-06-09 21:19:39 -0400

    Unfuck some more code?

--- src/kernel/drivers/irq.h
+++ src/kernel/drivers/irq.h
@@ -9,10 +9,10 @@ void irq_cleanup(void);
 #define IRQ_TEST(x, lo, hi) (((x) <= 32) ? (lo) & IRQ_MASK(x) : (hi) & IRQ_MASK(x - 32))
 #ifdef QEMU
 #define IRQ_TIMER 4
-#define IRQ_COM1 13 // UART 1
-#define IRQ_COM2 12 // UART 0
+#define IRQ_COM1 12
+#define IRQ_COM2 13
 #else
 #define IRQ_TIMER 51
-#define IRQ_COM1 52 // UART 0
-#define IRQ_COM2 54 // UART 1
+#define IRQ_COM1 52
+#define IRQ_COM2 54
 #endif
