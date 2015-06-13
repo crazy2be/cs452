20acdea98b2b39b4b19d8c37fce64c92505077c2
crazy2be <code@jmcgirr.com>
2015-06-13 13:27:10 -0400

    Address conreview suggestions.

--- src/kernel/tasks.c
+++ src/kernel/tasks.c
@@ -27,33 +27,41 @@ struct task_descriptor *task_create(void *entrypoint, int priority, int parent_t
 	// case gracefully.
 	KASSERT(task);
 	KFASSERT(task->state == DEAD, "%d", task->state);
-	int tid = task->tid + NUM_TID;
-	// TODO: Are stacks ascending or descending in memory? One of those makes
-	// this wrong (although it seems to _work_, but it might break other things)
-	void *sp = tasks.stacks[tid%NUM_TID + 1];
-	memset(sp - sizeof(tasks.stacks[0]), 0, sizeof(tasks.stacks[0]));
-	*task = (struct task_descriptor) {
-		.tid = tid,
-		.parent_tid = parent_tid,
-		.priority = priority,
-		.state = READY,
-	};
 
-	struct user_context *uc = ((struct user_context*) sp) - 1;
-	uc->pc = (unsigned) entrypoint;
-	uc->lr = (unsigned) &exitk;
-	// leave most everything else uninitialized
-	uc->r12 = (unsigned) sp;
+	int tid = task->tid + NUM_TID;
+	KFASSERT(tid_possible(tid), "%d", tid);
+	KASSERT((tid % NUM_TID) == (task - tasks.task_buf));
+
+	// Full descending stack, so we actually initialize the sp to point one
+	// element past the end of the stack allocated for the task, where the
+	// stack will grow "backwards" from there (never touching that space).
+	void *sp = tasks.stacks[tid%NUM_TID + 1];
+	// TODO: Measure performance of this. It would be nice to avoid some
+	// potentially nefarious bugs, but may be too slow to be practical.
+	memset(sp - sizeof(tasks.stacks[0]), 0, sizeof(tasks.stacks[0]));
+
 	unsigned cpsr;
 	__asm__ ("mrs %0, cpsr" : "=r" (cpsr));
 	cpsr &= 0xfffffff0; // set mode to user mode
 	/* cpsr |= 0x80; // turn interrupts on for this task */
 	cpsr &= ~0x80; // deassert the I bit to turn on interrupts
-	uc->cpsr = cpsr;
 
-	task->context = uc;
+	struct user_context *uc = ((struct user_context*) sp) - 1;
+	*uc = (struct user_context) {
+		.pc = (unsigned) entrypoint,
+		.lr = (unsigned) &exitk,
+		// leave most everything else uninitialized
+		.r12 = (unsigned) sp,
+		.cpsr = cpsr,
+	};
 
-	task->user_time_useconds = 0;
+	*task = (struct task_descriptor) {
+		.tid = tid,
+		.parent_tid = parent_tid,
+		.priority = priority,
+		.state = READY,
+		.context = uc,
+	};
 
 	return task;
 }
@@ -79,7 +87,7 @@ int tid_valid(int tid) {
 			&& tasks.task_buf[tid % NUM_TID].state != DEAD;
 }
 int tid_possible(int tid) {
-	return 1; // TODO: What should this be?
+	return tid >= 0; // TODO: What should this be?
 }
 int tid_next(void) {
 	return 256; // TODO: Should be task_queue_top()->tid + NUM_TID or something?
@@ -88,5 +96,5 @@ struct task_descriptor *task_from_tid(int tid) {
 	KASSERT(tid >= 0);
 	struct task_descriptor *task = &tasks.task_buf[tid % NUM_TID];
 	KASSERT(task->tid == tid); // Call tid_valid first to handle this gracefully.
-	return &tasks.task_buf[tid % NUM_TID];
+	return task;
 }
