219b3b41f4e149115e7625111360ebd2509c3e6e
Peter Raboud <praboud@gmail.com>
2015-06-16 16:23:44 -0400

    Get first cut of sensors working

--- src/user/main.c
+++ src/user/main.c
@@ -46,9 +46,9 @@ void init(void) {
 void test_init(void) {
 	start_servers();
 
-	/* sensorsrv(); */
-	displaysrv();
-	commandsrv();
+	sensorsrv();
+	/* displaysrv(); */
+	/* commandsrv(); */
 
 	/* printf("Hello world" EOL); */
 	/* char buf[] = {0xE2, 0x94, 0x90}; */
--- src/user/sensorsrv.c
+++ src/user/sensorsrv.c
@@ -7,7 +7,7 @@
 
 void sensor_set(struct sensor_state *s, int num, int tripped) {
 	const int word_num = num / 8;
-	const int offset = num % 8;
+	const int offset = 7 - num % 8;
 	const int bit = 0x1 << offset;
 
 	ASSERT(word_num <= sizeof(s->packed) / sizeof(s->packed[0]));
@@ -19,7 +19,7 @@ void sensor_set(struct sensor_state *s, int num, int tripped) {
 
 int sensor_get(struct sensor_state *s, int num) {
 	const int word_num = num / 8;
-	const int offset = num % 8;
+	const int offset = 7 - num % 8;
 	const int bit = 0x1 << offset;
 
 	ASSERT(word_num <= sizeof(s->packed) / sizeof(s->packed[0]));
@@ -27,10 +27,27 @@ int sensor_get(struct sensor_state *s, int num) {
 	return s->packed[word_num] & bit;
 }
 
+// 4-byte buf (including 1 byte of null-term)
+void sensor_repr(int n, char *buf) {
+	// this is a hack, since I don't want to implement general sprintf right now
+	const int group = n / 16;
+	int number = (n % 16) + 1;
+	*buf++ = 'A' + group;
+	if (number >= 10) {
+		*buf++ = '1';
+		number -= 10;
+	}
+	*buf++ = '0' + number;
+	*buf++ = '\0';
+}
+
 void start_sensorsrv(void) {
 	struct sensor_state sensors, old_sensors;
+	char buf[4];
 
 	memset(&old_sensors, 0, sizeof(old_sensors));
+
+	printf("Starting sensorsrv" EOL);
 
 	for (;;) {
 		send_sensor_poll();
@@ -39,7 +56,8 @@ void start_sensorsrv(void) {
 		for (int i = 0; i < 80; i++) {
 			int status = sensor_get(&sensors, i);
 			if (status != sensor_get(&old_sensors, i)) {
-				printf("Saw sensor %d change to %d" EOL, i, status);
+				sensor_repr(i, buf);
+				printf("Saw sensor %s change to %d" EOL, buf, status);
 			}
 		}
 
