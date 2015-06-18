3c2d9f8a142d854003a949aaea6744a6d9bb94cd
crazy2be <code@jmcgirr.com>
2015-06-18 18:34:11 -0400

    Rotating.

--- src/mock-train/main.py
+++ src/mock-train/main.py
@@ -6,6 +6,14 @@ import math
 
 import pygame
 
+def deg_to_rad(deg): return deg * math.pi / 180
+def rot_center(image, rect, angle):
+	"""rotate an image while keeping its center"""
+	# http://www.pygame.org/wiki/RotateCenter
+	rot_image = pygame.transform.rotate(image, angle)
+	rot_rect = rot_image.get_rect(center=rect.center)
+	return rot_image,rot_rect
+
 class Game(object):
 	def __init__(self, surface):
 		self.surface = surface
@@ -15,15 +23,25 @@ class Game(object):
 		font = pygame.font.Font(None, 36)
 		text = font.render("Hello There", 1, (10, 10, 10))
 
+		rot = 45
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					return
 			self.surface.fill((255, 255, 255))
+			surf = pygame.Surface((100, 50))
+			surf.fill((0, 0, 0))
+			surf.set_colorkey((0, 0, 0))
+			rect = pygame.Rect(0, 0, 100, 50)
+			pygame.draw.rect(surf, (100, 0, 0), rect)
+			surf2, rect2 = rot_center(surf, rect, rot)
+			rot -= 1
+			self.surface.blit(surf2, rect2.move(100, 100))
 			textpos = text.get_rect()
 			textpos.centerx = self.surface.get_rect().centerx
 			self.surface.blit(text, textpos)
 			pygame.display.flip()
+			self.clock.tick(30)
 
 	def play_game(self):
 		self.start_screen()
@@ -33,7 +51,7 @@ def main():
 
 	surface = pygame.display.set_mode([800, 600])
 	pygame.mouse.set_visible(False)
-	pygame.display.set_caption("Argh, it's the Asteroids!!")
+	pygame.display.set_caption("Trains")
 
 	game = Game(surface)
 
