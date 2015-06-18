5db2ac223df1daaee82ee552175d09a380040601
Peter Raboud <praboud@gmail.com>
2015-06-17 21:43:12 -0400

    WIP on better switch updates

--- src/user/trainsrv.h
+++ src/user/trainsrv.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// TODO: later, we may want to add left/right distinction, to
-// simplify dealing with the tri-state switches on the track
-enum sw_direction { STRAIGHT, CURVED };
+#include "switch_state.h"
 
 int trains_set_speed(int train, int speed);
 int trains_reverse(int train);
