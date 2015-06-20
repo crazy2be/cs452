dbfa653371c3fed0b8426afe635a26a999058596
crazy2be <code@jmcgirr.com>
2015-06-20 16:47:55 -0400

    Objects = good, right?

--- src/mock-train/main.py
+++ src/mock-train/main.py
@@ -14,6 +14,31 @@ def rot_center(image, rect, angle):
 	rot_rect = rot_image.get_rect(center=rect.center)
 	return rot_image,rot_rect
 
+class Train():
+	def __init__(self):
+		surf = pygame.Surface((100, 50))
+		surf.fill((0, 0, 0))
+		surf.set_colorkey((0, 0, 0))
+		self.raw_image = surf
+		self.raw_rect = pygame.Rect(0, 0, 100, 50)
+		self.rot = 0
+		self.off = (100, 100)
+		pygame.draw.rect(surf, (100, 0, 0), self.raw_rect)
+
+	def rotate(self, amount):
+		self.rot += amount
+		self.image, self.rect = rot_center(self.raw_image, self.raw_rect, self.rot)
+
+	def move(self, amount):
+		self.off = (self.off[0] + amount[0], self.off[1] + amount[1])
+
+	def update(self):
+		self.rotate(1)
+		self.move((random.randint(-5, 5), random.randint(-5, 5)))
+
+	def draw(self, surf):
+		surf.blit(self.image, self.rect.move(self.off))
+
 class Game(object):
 	def __init__(self, surface):
 		self.surface = surface
@@ -24,19 +49,14 @@ class Game(object):
 		text = font.render("Hello There", 1, (10, 10, 10))
 
 		rot = 45
+		train = Train()
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					return
 			self.surface.fill((255, 255, 255))
-			surf = pygame.Surface((100, 50))
-			surf.fill((0, 0, 0))
-			surf.set_colorkey((0, 0, 0))
-			rect = pygame.Rect(0, 0, 100, 50)
-			pygame.draw.rect(surf, (100, 0, 0), rect)
-			surf2, rect2 = rot_center(surf, rect, rot)
-			rot -= 1
-			self.surface.blit(surf2, rect2.move(100, 100))
+			train.update()
+			train.draw(self.surface)
 			textpos = text.get_rect()
 			textpos.centerx = self.surface.get_rect().centerx
 			self.surface.blit(text, textpos)
