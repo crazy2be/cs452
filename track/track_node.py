40989659d774511f92f86b01e1f0263cd5d907e4
crazy2be <code@jmcgirr.com>
2015-06-27 16:42:10 -0400

    Add ability for parse_track to generate python.

---
+++ track/track_node.py
@@ -0,0 +1,24 @@
+NODE_NONE = 0
+NODE_SENSOR = 1
+NODE_BRANCH = 2
+NODE_MERGE = 3
+NODE_ENTER = 4
+NODE_EXIT = 5
+
+DIR_AHEAD = 0
+DIR_STRAIGHT = 0
+DIR_CURVED = 1
+
+class TrackEdge():
+  reverse = None # TrackEdge
+  src = None # TrackNode
+  dest = None #TrackNode
+  dist = 0 # millimeters
+
+class TrackNode():
+  name = "no name"
+  typ = -1
+  num = -1
+  reverse = None # TrackNode in the same location, but opposite direction.
+  edge = [TrackEdge(), TrackEdge()]
+
