e45e98e9b655c3960bf735327f75cece795edfdc
crazy2be <code@jmcgirr.com>
2015-07-22 19:54:19 -0400

    Fix the horribly broken A* code. Fancy features == BAD!

--- src/user/routesrv.h
+++ src/user/routesrv.h
@@ -6,4 +6,5 @@ void routesrv_start(void);
 
 // Blocking call
 int routesrv_plan(const struct track_node *start, const struct track_node *end,
-	struct astar_node (*path_out)[ASTAR_MAX_PATH]);
+	struct astar_node *path_out);
+
