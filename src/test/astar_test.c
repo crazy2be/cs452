147b961a9bc573a566d4cd915b44ac34a2322732
crazy2be <code@jmcgirr.com>
2015-07-22 15:21:19 -0400

    Add some stuff I forgot.

---
+++ src/test/astar_test.c
@@ -0,0 +1,12 @@
+#include "astar_test.h"
+
+#include "../lib/astar.h"
+
+void astar_tests(void) {
+	init_tracka(track);
+	{
+		struct astar_node path[ASTAR_MAX_PATH];
+		int l = astar_find_path(&track[0], &track[1], &path);
+		astar_print_path(&path, l);
+	}
+}
