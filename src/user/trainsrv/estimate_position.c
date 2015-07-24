87080f2fd581dc8db5a66a481b842a2af5a605ab
Peter Raboud <praboud@gmail.com>
2015-07-24 18:48:00 -0400

    Estimation with stopping curve works

--- src/user/trainsrv/estimate_position.c
+++ src/user/trainsrv/estimate_position.c
@@ -235,8 +235,12 @@ int update_train_speed(struct trainsrv_state *state, int train_id, int speed) {
 	// (the trains moving when the program starts, and then it refuses to stop them,
 	// since it thinks they're already stopped.) 
 	/* if (train_state->current_speed_setting == speed) return 0; */
+
+	// only reanchor if we have history (otherwise it crashes)
+	if (train_state->speed_history.len > 0) {
+		reanchor(state, train_state);
+	}
 	speed_historical_set(&train_state->speed_history, speed, time());
-	reanchor(state, train_state); // TODO: Deacceration model.
 
 	ASSERT(train_state->speed_history.len > 0);
 	return 1;
